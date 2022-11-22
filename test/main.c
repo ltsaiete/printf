e39e63c3768a9d54d5c1f739bffa26591056b4bb
Luis Saiete <luistsaiete@gmail.com>
2022-11-22 23:58:15 +0200

    Move main.c to test folder

--- test/main.c
+++ test/main.c
@@ -1,11 +1,19 @@
-#include "../main.h"
+#include "main.h"
 
 int main(void)
 {
-	char c;
+	char c, h, i, j, k;
+	char *s = "Exemplo";
 
-	c = 'E';
+	c = 'H';
+	h = 'E';
+	i = 'L';
+	j = 'L';
+	k = 'O';
 
-	_printf("Printing a char: %c %d %c  dfsfdfds %.4d dd", c);
+
+	_printf("Printing a char: %c, %c, %c, %c, %c\n", c, h, i, j, k);
+	_printf("Printing a string: %s\n", s);
+	_printf("Print nothing %s\n");
 	return (0);
 }
