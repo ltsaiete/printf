27b0d5ad23adb5c85214e41144b67ab1002d7a00
Uss-Momas <ussumane.momade@uem.ac.mz>
2022-11-23 22:41:41 +0200

    Add more Tests

---
+++ test/main_3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../main.h"
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success, error code otherwise
+ */
+int main(void)
+{
+	int len, len2 = 0;
+
+	len = _printf("Let'see if the cast is correctly done: %c. Did it work?\n", 48);
+	len2 = printf("Let'see if the cast is correctly done: %c. Did it work?\n", 48);
+	printf("\nLength: %d\n", len + len2);
+	fflush(stdout);
+	if (len != len2)
+	{
+		printf("Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+	return (0);
+}
